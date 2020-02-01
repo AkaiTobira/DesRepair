759657f9e855586e6099d80f2f4c47de61f3074c
AkaiTobira <techmaturgia.data@student.uj.edu.pl>
2020-02-01 17:28:54 +0100

    Add Gates

---
+++ Scenes/Root.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+var doors_and_keys = { }
+
+
+func _ready():
+	
+	for child in get_children(): 
+		if "Key" in child.get_groups():
+			if not doors_and_keys.has( child.key_id ): doors_and_keys[ child.key_id ] = {}
+			if not doors_and_keys[ child.key_id ].has( "keys" ) : doors_and_keys[ child.key_id][ "keys" ] = []
+			doors_and_keys[ child.key_id][ "keys" ].append( child )
+			doors_and_keys[child.key_ide][ "is_open"] = false
+		if "Door" in child.get_groups():
+			print( "NOHtinG" )
+			if not doors_and_keys.has( child.door_id ): doors_and_keys[ child.door_id ] = {}
+			if not doors_and_keys[ child.door_id ].has( "door" ) : doors_and_keys[ child.door_id][ "door" ] = []
+			doors_and_keys[ child.door_id][ "door" ].append( child )
+			doors_and_keys[child.key_ide][ "is_open"] = false
+
+func unlock_doors( key_id ):
+	if doors_and_keys[key_id][ "is_open"] : return
+	doors_and_keys[key_id][ "is_open"] = true
+	for door in doors_and_keys[ key_id ]["door"]:
+		door.get_node("AnimationPlayer").play("Open")
+
---
+++ Scripts/Key.gd
@@ -0,0 +1,39 @@
+extends Area2D
+
+export var premium = ""
+
+var player
+export var key_id = -1
+
+func _ready():
+	pass # Replace with function body.
+
+
+
+func on_heal():
+	player.heal_cost( premium )
+	get_parent().unlock_doors( key_id )
+	player.get_node("Camera2D").zoom = Vector2(2,2)
+	$Buttons.queue_free()
+	set_process(false)
+
+func _process(delta):
+	if not $Buttons.visible: return
+
+	if Input.is_action_just_pressed("heal"): on_heal()
+
+func _on_broken_robot_body_entered(body):
+	if "player" in body.get_groups():
+		player = body
+		if not has_node("Buttons"): return
+		body.get_node("Camera2D").zoom = Vector2(0.6,0.6)
+		$Buttons.visible = true
+		$Buttons/AnimationPlayer.play("migaj")
+
+func _on_broken_robot_body_exited(body):
+	if "player" in body.get_groups():
+		player = body
+		body.get_node("Camera2D").zoom = Vector2(2,2)
+		if not has_node("Buttons"): return
+		$Buttons.visible = false
+		$Buttons/AnimationPlayer.stop()
