c863ffed25895f65aa16c11851b795fd5f70bf37
Daniel Kowalewski <tenshey10@gmail.com>
2020-02-02 03:56:48 +0100

    scraptest failed

---
+++ Scripts/Scraptest.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var VALUE = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func _on_Scrap_body_entered(body):
+	if "player" in body.get_groups():
+		body.add_to_score(VALUE)
+		#$AnimationPlayer.play( play deasapear animation )
+		call_deferred("queue_free" ) 
+	pass # Replace with function body.
